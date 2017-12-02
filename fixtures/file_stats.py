23882ea124d4e2eec00ca9b7ab775f257679fa97
cynthia <cdisenfeld@gmail.com>
2017-12-02 18:02:28 +0000

    pylint corrections

--- fixtures/file_stats.py
+++ fixtures/file_stats.py
@@ -1,20 +1,25 @@
-class FileStatsException(Exception):
-	pass
-		
+''' FileStats related classes'''
 
-class FileStats:
-	def __init__(self, curr_file):
-		if not curr_file:
-			raise FileStatsException
-		self.curr_file = curr_file
-	
-	def count_lines(self):
-		with open(self.curr_file) as open_file:
-			contents = open_file.readlines()
-		return len(contents)
-	
-	def count_chars(self):
-		with open(self.curr_file) as open_file:
-			contents = open_file.read()
-		return len(contents)
-	
+class FileStatsException(Exception):
+    ''' Exception class for anything related to FileStats'''
+    pass
+
+
+class FileStats(object):
+    ''' FileStats class calculates some metrics from a given file'''
+    def __init__(self, curr_file):
+        if not curr_file:
+            raise FileStatsException
+        self.curr_file = curr_file
+
+    def count_lines(self):
+        ''' returns the number of lines in the current file'''
+        with open(self.curr_file) as open_file:
+            contents = open_file.readlines()
+        return len(contents)
+
+    def count_chars(self):
+        ''' returns the number of characters in the current file'''
+        with open(self.curr_file) as open_file:
+            contents = open_file.read()
+        return len(contents)
--- fixtures/test_file_stats.py
+++ fixtures/test_file_stats.py
@@ -1,3 +1,4 @@
+''' FileStats unit tests '''
 import os
 import pytest
 
@@ -5,37 +6,43 @@ from file_stats import FileStats, FileStatsException
 
 @pytest.fixture
 def empty_file():
-	filename = "empty_file.txt"
-	open(filename, 'w').close()
-	yield filename
-	os.remove(filename)
+    '''fixture to define an empty file'''
+    filename = "empty_file.txt"
+    open(filename, 'w').close()
+    yield filename
+    os.remove(filename)
 
 @pytest.fixture
 def lines2_chars4_file():
-	filename = "non_empty_file.txt"
-	with open(filename, "w") as open_file:
-		open_file.write("1\n")
-		open_file.write("2\n")
-	yield filename
-	os.remove(filename)
-	
+    '''fixture to define a file with 2 lines and 4 characters'''
+    filename = "non_empty_file.txt"
+    with open(filename, "w") as open_file:
+        open_file.write("1\n")
+        open_file.write("2\n")
+    yield filename
+    os.remove(filename)
 
 def test_no_given_file():
-	with pytest.raises(FileStatsException):
-		FileStats(None)
+    '''test should raise an exception when no file is given'''
+    with pytest.raises(FileStatsException):
+        FileStats(None)
 
 def test_lines_empty_file(empty_file):
-	fs = FileStats(empty_file)
-	assert fs.count_lines() == 0
+    '''test to check lines of empty file'''
+    file_stats = FileStats(empty_file)
+    assert file_stats.count_lines() == 0
 
 def test_chars_empty_file(empty_file):
-	fs = FileStats(empty_file)
-	assert fs.count_chars() == 0
+    '''test to check characters of empty file'''
+    file_stats = FileStats(empty_file)
+    assert file_stats.count_chars() == 0
 
 def test_lines_non_empty_file(lines2_chars4_file):
-	fs = FileStats(lines2_chars4_file)
-	assert fs.count_lines() == 2
+    '''test to check lines of non-empty file'''
+    file_stats = FileStats(lines2_chars4_file)
+    assert file_stats.count_lines() == 2
 
 def test_chars_non_empty_file(lines2_chars4_file):
-	fs = FileStats(lines2_chars4_file)
-	assert fs.count_chars() == 4
+    '''test to check characters of non-empty file'''
+    file_stats = FileStats(lines2_chars4_file)
+    assert file_stats.count_chars() == 4
